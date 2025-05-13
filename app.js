268d07303294f4f1304be38dda8221f3d6c3785b
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2025-05-13 09:29:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let code = document.getElementById("code");
+let charcters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}><.,?~";
+let pass = "";
+
+function gen (){
+}
+gen()
+for (let i = 0; i < length; i++) {
+    let genrate = Math.floor(Math.random(i));
+    console.log(genrate);
+    
+    
+}
